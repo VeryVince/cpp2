fc7f886961b93984ed8afe0d572cdeb93b6889a3
VeryVince <12@34.56>
2021-05-02 22:33:49 -0400

    removed round, cleaned up, added note

--- 15bTemplates/HelloWorld.cpp
+++ 15bTemplates/HelloWorld.cpp
@@ -2,18 +2,15 @@
     Your Name: Vincent Apanovitch
     Date: 5/2/21
     Program Name: CIS 1202
-    Description:
-    Resources:
+    Notes: Looks like I still have to push using vs code! Legit wayyy too frustrating.
 */
 
 #include <iostream>
-#include <math.h>
-#include <string>
 using namespace std;
 
 template <class T>
 T half(T full) {return full / 2;}
-int half(int full) { return round(full / 2.0); }
+int half(int full) { return ((full%2)? full+1 : full) / 2; } //I don't need no stinking round!!!! LOL, realized last sec
 
 int main()
 {
@@ -21,19 +18,8 @@ int main()
     double myD = (double) 8;
     int myI = 13;
 
-    cout << half(myF) << "\t" << half(myD) << "\t" << half(myF);
+    cout << half(myF) << "\t" << half(myD) << "\t" << half(myI) << endl;
 
     system("pause");
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
